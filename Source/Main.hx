dca1ce0c7ed964e5f0bf7e4475bdd950047ed834
Misiur <misiur66+github@gmail.com>
2016-10-15 16:10:13 +0200

    OpenFL bug

---
+++ Source/Main.hx
@@ -0,0 +1,17 @@
+package;
+
+import openfl.display.Bitmap;
+import openfl.display.Sprite;
+import openfl.display.Shape;
+
+class Main extends Sprite {
+	public function new() {
+		super();
+	}
+}
+
+class Foo extends Shape {
+	public function new() {
+		super();
+	}
+}
